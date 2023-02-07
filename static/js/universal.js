631bf1983b159c37467fdcd2dd2d725163452e87
Iain Emsley <Iain.Emsley@warwick.ac.uk>
2023-02-07 21:09:58 +0000

    Added colours

--- static/js/universal.js
+++ static/js/universal.js
@@ -1,6 +1,24 @@
 var map;
-function createMap(lat, lon) {
-    map = L.map('map').setView([lat, lon], 15);
+function createMap(lat, lon, depth) {
+    map = L.map('map').setView([lat, lon], depth);
 
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'}).addTo(map);
+}
+
+function getColour(colour) {
+    const tags = {
+        'Technology': "blue",
+        'entertainment': 'red',
+        'Fitness': "red",
+        'Smart home' : "red", 
+        "Music": "red",
+        "Security": "blue", 
+        "Medical": "grey", 
+        "Analytics": "blue", 
+        "Storage": "blue", 
+        "automotive": "yellow", 
+        "Household": "red" 
+      };
+
+      return tags[colour];
 }
